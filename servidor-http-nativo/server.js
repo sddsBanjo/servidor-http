0b2bca40c213078332f9845b488d2727ad8792df
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-08-14 21:01:32 +0000

    edit: (week 2 and 3) exercise 4

--- servidor-http-nativo/server.js
+++ servidor-http-nativo/server.js
@@ -11,6 +11,12 @@ server.on('request', (req, res) => {
     res.statusCode = 201;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
     res.end(JSON.stringify({ status: "ok" }));
+    /*
+    Não vou remover o res.end pra não comprometer o funcionamento do código, mas resumindo:
+    Sem o res.end, o navegador fica carregando eternamente. A conexão fica pendente, onde o
+    cliente fica esperando indefinidamente por uma resposta que o servidor nunca entrega.
+    Depois de certo tempo, o navegador dá timeout e desiste.
+    */
 });
 
 server.listen(porta, () => {
